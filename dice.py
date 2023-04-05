ece13d3ce78b4abbdeae080ca75ed2f479ff969b
Alex Leung <96800981+AlexLTK1@users.noreply.github.com>
2023-04-04 20:45:55 -0700

    Create dice.py

---
+++ dice.py
@@ -0,0 +1,10 @@
+import random
+
+# define the number of sides on the dice
+num_sides = 6
+
+# roll the dice
+roll = random.randint(1, num_sides)
+
+# print the result
+print(f"You rolled a {roll}!")
