05d4bb22d53016baeb5cd4bece6eeaf0f56e3e99
Alex Leung <96800981+AlexLTK1@users.noreply.github.com>
2023-04-13 20:44:08 -0700

    fixed the gui

--- gameautomation.py
+++ gameautomation.py
@@ -1,55 +1,53 @@
 import time
 import pyautogui
 import tkinter as tk
+import os
 
 class App:
     def __init__(self, master):
         self.master = master
-        master.title("Automation Tool")
-
-        self.delay_label = tk.Label(master, text="Delay (seconds):")
-        self.delay_label.grid(row=0, column=0)
-
+        self.delay = 5.0
+        master.title("Game Automation")
+        tk.Label(master, text="Delay (in seconds):").grid(row=0, column=0)
         self.delay_entry = tk.Entry(master)
-        self.delay_entry.insert(0, "2.5")
         self.delay_entry.grid(row=0, column=1)
+        self.delay_entry.insert(0, str(self.delay))
+        tk.Button(master, text="Save", command=self.save_delay).grid(row=1, column=1)
+        tk.Button(master, text="Start", command=self.start_automation).grid(row=2, column=0)
+        tk.Button(master, text="Stop", command=self.stop_automation).grid(row=2, column=1)
+        tk.Button(master, text="Quit", command=self.quit).grid(row=2, column=2)
 
-        self.save_button = tk.Button(master, text="Save", command=self.save_delay)
-        self.save_button.grid(row=1, column=0)
+    def start_automation(self):
+        self.is_running = True
+        while self.is_running:
+            pyautogui.click(pyautogui.position())
+            time.sleep(self.delay)
+
+    def stop_automation(self):
+        self.is_running = False
 
     def save_delay(self):
-        delay = float(self.delay_entry.get())
+        self.delay = float(self.delay_entry.get())
+        with open("delay.txt", "w") as f:
+            f.write(str(self.delay))
 
+    def quit(self):
         # Save the delay to a file
         with open("delay.txt", "w") as f:
-            f.write(str(delay))
-
+            f.write(str(self.delay))
         self.master.destroy()
 
 if __name__ == '__main__':
     # Load the delay from a file
-    with open("delay.txt", "r") as f:
-        delay = float(f.read())
+    if os.path.exists("delay.txt"):
+        with open("delay.txt", "r") as f:
+            delay = float(f.read())
+    else:
+        delay = 5.0
+        with open("delay.txt", "w") as f:
+            f.write(str(delay))
 
     # Create the GUI
     root = tk.Tk()
     app = App(root)
     root.mainloop()
-
-    # Delay before starting the automation
-    time.sleep(delay)
-
-    # Loop to automate the task
-    for i in range(10):
-        # Move the mouse to the desired location and click
-        pyautogui.moveTo(100, 200, duration=0.5)
-        pyautogui.click()
-
-        # Type a message
-        pyautogui.typewrite("Hello World!")
-
-        # Press the Enter key
-        pyautogui.press('enter')
-
-        # Wait for the specified delay before repeating
-        time.sleep(delay)
